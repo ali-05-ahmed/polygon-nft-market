c7d6c54e3d62877677725f2fa7775484906bb5bf
ali <extricateshareware@gmail.com>
2021-12-07 01:11:11 +0500

    test updated

--- backend/test/sample-test.js
+++ backend/test/sample-test.js
@@ -36,129 +36,163 @@ describe("NFTMarket", function () {
     //createAuction(uint256 _basePrice, uint256 _endingUnix, address _nftContract, uint256 _tokenId, address _msgSender) public
   })
 
-  it("Should create and execute Auction", async function () {
-    const _value = await ethers.utils.parseUnits('10', 'wei')
-    const _value2 = await ethers.utils.parseUnits('15', 'wei')
-    const _value3 = await ethers.utils.parseUnits('5', 'wei')
+  // it("Should create and execute Auction", async function () {
+  //   const _value = await ethers.utils.parseUnits('10', 'wei')
+  //   const _value2 = await ethers.utils.parseUnits('15', 'wei')
+  //   const _value3 = await ethers.utils.parseUnits('5', 'wei')
 
-    let addr = await market.auction_addr()
+  //   let addr = await market.auction_addr()
 
-    auction = await Auction.attach(addr)
+  //   auction = await Auction.attach(addr)
 
-    let mint = await nft.createToken("asd")
+  //   let mint = await nft.createToken("asd")
+  //   await mint.wait()
+
+  //   let create = await market.createMarketItem(nft.address, 1, 1, true, 10)
+
+  //   await create.wait()
+  //   let bid = await auction.connect(person1).bid(nft.address, 1, { value: _value3 })
+  //   await bid.wait()
+  //   bid = await auction.connect(person3).bid(nft.address, 1, { value: _value })
+  //   await bid.wait()
+  //   bid = await auction.connect(person2).bid(nft.address, 1, { value: _value2 })
+  //   await bid.wait()
+  //   await new Promise(resolve => setTimeout(resolve, 11000));
+  //   bid = await auction.connect(person2).bid(nft.address, 1, { value: _value2 })
+  //   await bid.wait()
+
+
+
+  //   let highestBid = await auction.getHighestBid(nft.address, 1)
+  //   console.log(highestBid.toNumber())
+
+  //   let pending = await auction.connect(person2).getPendingReturns()
+  //   console.log(pending.toNumber())
+
+  //   let sell = await market.connect(person2).createMarketSale(nft.address, 1)
+  //   await sell.wait()
+
+
+  //   mint = await nft.createToken("asd")
+  //   await mint.wait()
+
+  //   create = await market.createMarketItem(nft.address, 2, 1, true, 10)
+
+  //   bid = await auction.connect(person1).bid(nft.address, 2, { value: _value3 })
+  //   await bid.wait()
+  //   bid = await auction.connect(person3).bid(nft.address, 2, { value: _value })
+  //   await bid.wait()
+  //   bid = await auction.connect(person2).bid(nft.address, 2, { value: _value2 })
+  //   await bid.wait()
+  //   await new Promise(resolve => setTimeout(resolve, 11000));
+  //   bid = await auction.connect(person2).bid(nft.address, 2, { value: _value3 })
+  //   await bid.wait()
+
+  //   highestBid = await auction.getHighestBid(nft.address, 1)
+  //   console.log(highestBid.toNumber())
+
+  //   pending = await auction.connect(person1).getPendingReturns()
+  //   console.log(pending.toNumber())
+
+
+  // })
+
+
+  // it("generate collection", async function () {
+
+  //   let ss = await factory.connect(person1).createCollection("mynft", "nn", "www.google.com/");
+  //   await ss.wait()
+  //   let collection = await Collection.attach()
+  //   console.log("person 1 ", person1.address)
+  //   console.log("collection", ss)
+  // })
+  // it("get collection URI", async function () {
+
+  //   let col1 = await factory.ownerIdToCollection(person1.address, 0)
+  //   console.log("collection 1", col1)
+  //   let collection = await Collection.attach(col1,)
+
+
+  //   let mint = await collection.connect(person1).createToken("first")
+  //   await mint.wait()
+  //   let URI = await collection.tokenURI(1)
+  //   console.log("collection", URI)
+
+  // })
+
+  // it("generate collection", async function () {
+
+  //   let ss = await factory.connect(person1).createCollection("mynft", "nn", "www.google.com/");
+  //   await ss.wait()
+  //   console.log("person 1 ", person1.address)
+  //   console.log("collection", ss.data)
+  // })
+
+  // it("check collection count", async function () {
+
+  //   let ss = await factory.collections(_.address);
+  //   console.log("collection", ss.toNumber())
+  // })
+  // it("check collection count", async function () {
+
+  //   let ss = await factory.collections(person1.address);
+  //   console.log("collection", ss.toNumber())
+  // })
+  // it("check collection address", async function () {
+  //   let col1 = await factory.ownerIdToCollection(person1.address, 0)
+  //   console.log("collection 1", col1)
+  //   col1 = await factory.ownerIdToCollection(person1.address, 1)
+  //   console.log("collection 2", col1)
+  //   col1 = await factory.ownerIdToCollection(person1.address, 2)
+  //   console.log("collection 3", col1)
+  // })
+  // it("generate collection", async function () {
+
+  //   let ss = await factory.connect(person1).createCollection("mynft", "nn", "www.google.com/");
+  //   await ss.wait()
+  //   console.log("person 1 ", person1.address)
+  //   console.log("collection", ss.data)
+  // })
+  // it("check collection address", async function () {
+  //   let col1 = await factory.ownerIdToCollection(person1.address, 0)
+  //   console.log("collection 1", col1)
+  //   col1 = await factory.ownerIdToCollection(person1.address, 1)
+  //   console.log("collection 2", col1)
+  //   col1 = await factory.ownerIdToCollection(person1.address, 2)
+  //   console.log("collection 3", col1)
+  // })
+  it("generate collection and mint", async function () {
+
+    let ss = await factory.createCollection("mynft", "nn", "www.googlsajdlalsdlae.com/");
+    await ss.wait()
+    let noCol = await factory.collections(_.address);
+    let col1 = await factory.ownerIdToCollection(_.address, 0)
+    collection = await Collection.attach(col1)
+
+    let mint = await collection.createToken("asdasd")
     await mint.wait()
 
-    let create = await market.createMarketItem(nft.address, 1, 1, true, 10)
-
-    await create.wait()
-    let bid = await auction.connect(person1).bid(nft.address, 1, { value: _value3 })
-    await bid.wait()
-    bid = await auction.connect(person3).bid(nft.address, 1, { value: _value })
-    await bid.wait()
-    bid = await auction.connect(person2).bid(nft.address, 1, { value: _value2 })
-    await bid.wait()
-    await new Promise(resolve => setTimeout(resolve, 11000));
-    bid = await auction.connect(person2).bid(nft.address, 1, { value: _value2 })
-    await bid.wait()
-
-
-
-    let highestBid = await auction.getHighestBid(nft.address, 1)
-    console.log(highestBid.toNumber())
-
-    let pending = await auction.connect(person2).getPendingReturns()
-    console.log(pending.toNumber())
-
-    let sell = await market.connect(person2).createMarketSale(nft.address, 1)
-    await sell.wait()
-
-
-    mint = await nft.createToken("asd")
+    mint = await collection.createToken("xzzc")
     await mint.wait()
 
-    create = await market.createMarketItem(nft.address, 2, 1, true, 10)
+    let balance = await collection.balanceOf(_.address)
+    console.log("NO OF NFTs ", balance.toNumber())
 
-    bid = await auction.connect(person1).bid(nft.address, 2, { value: _value3 })
-    await bid.wait()
-    bid = await auction.connect(person3).bid(nft.address, 2, { value: _value })
-    await bid.wait()
-    bid = await auction.connect(person2).bid(nft.address, 2, { value: _value2 })
-    await bid.wait()
-    await new Promise(resolve => setTimeout(resolve, 11000));
-    bid = await auction.connect(person2).bid(nft.address, 2, { value: _value3 })
-    await bid.wait()
+    let NFTs1 = await collection.tokenOfOwnerByIndex(_.address, 0)
+    console.log(NFTs1.toNumber())
+    let URI = await collection.tokenURI(NFTs1);
+    console.log(URI)
 
-    highestBid = await auction.getHighestBid(nft.address, 1)
-    console.log(highestBid.toNumber())
-
-    pending = await auction.connect(person1).getPendingReturns()
-    console.log(pending.toNumber())
+    let NFTs2 = await collection.tokenOfOwnerByIndex(_.address, 1)
+    console.log(NFTs2.toNumber())
+    URI = await collection.tokenURI(NFTs2);
+    console.log(URI)
 
 
-  })
 
 
-  it("generate collection", async function () {
-
-    let ss = await factory.connect(person1).createCollection("mynft", "nn", "www.google.com/");
-    await ss.wait()
-    console.log("person 1 ", person1.address)
-    console.log("collection", ss)
-  })
-  it("get collection URI", async function () {
-
-    let col1 = await factory.ownerIdToCollection(person1.address, 0)
-    console.log("collection 1", col1)
-    let collection = await Collection.attach(col1,)
 
 
-    let mint = await collection.connect(person1).createToken("first")
-    await mint.wait()
-    let URI = await collection.tokenURI(1)
-    console.log("collection", URI)
-
-  })
-
-  it("generate collection", async function () {
-
-    let ss = await factory.connect(person1).createCollection("mynft", "nn", "www.google.com/");
-    await ss.wait()
-    console.log("person 1 ", person1.address)
-    console.log("collection", ss.data)
-  })
-
-  it("check collection count", async function () {
-
-    let ss = await factory.collections(_.address);
-    console.log("collection", ss.toNumber())
-  })
-  it("check collection count", async function () {
-
-    let ss = await factory.collections(person1.address);
-    console.log("collection", ss.toNumber())
-  })
-  it("check collection address", async function () {
-    let col1 = await factory.ownerIdToCollection(person1.address, 0)
-    console.log("collection 1", col1)
-    col1 = await factory.ownerIdToCollection(person1.address, 1)
-    console.log("collection 2", col1)
-    col1 = await factory.ownerIdToCollection(person1.address, 2)
-    console.log("collection 3", col1)
-  })
-  it("generate collection", async function () {
-
-    let ss = await factory.connect(person1).createCollection("mynft", "nn", "www.google.com/");
-    await ss.wait()
-    console.log("person 1 ", person1.address)
-    console.log("collection", ss.data)
-  })
-  it("check collection address", async function () {
-    let col1 = await factory.ownerIdToCollection(person1.address, 0)
-    console.log("collection 1", col1)
-    col1 = await factory.ownerIdToCollection(person1.address, 1)
-    console.log("collection 2", col1)
-    col1 = await factory.ownerIdToCollection(person1.address, 2)
-    console.log("collection 3", col1)
   })
 
 })
